2d035a4270e60ac861cda41b404f3ff28110c801
blacktost <dbstjd100@naver.com>
2021-12-13 07:11:59 +0000

    feat(ClipEvnets): 저장된 영상 삭제 이벤트
    - 로컬스토리지에서 해당 클립 데이터 삭제 기능 완료
    - 재 렌더링기능 필요

--- src/js/handler/onMainClipEvents.js
+++ src/js/handler/onMainClipEvents.js
@@ -1,4 +1,6 @@
+import { LOCAL_STORAGE_KEYS } from '../utils/constant.js';
 import { $ } from '../utils/dom.js';
+import { getItem, setItem } from '../utils/store.js';
 /**
  * [] 휴지통 눌렀을때 저장된 클립 삭제
  * [] 체크 이모지 눌렀을때 본 클립으로 이동?
@@ -7,7 +9,23 @@ import { $ } from '../utils/dom.js';
 const $savedClipWrapper = $('.main-savedClip-wrapper');
 
 function handleDeleteSavedClip(target) {
-	console.log(target);
+	let clipId = target.closest('.main-youtube-savedClip');
+	let clipNumber = clipId.dataset.clipId;
+	let storedId = getItem(LOCAL_STORAGE_KEYS.STORE_CLIP_ID);
+	let storedClips = getItem(LOCAL_STORAGE_KEYS.STORE_CLIP_INFO);
+	storedId.splice(clipNumber, 1);
+	storedClips.splice(clipNumber, 1);
+	setItem(LOCAL_STORAGE_KEYS.STORE_CLIP_INFO, storedClips);
+	setItem(LOCAL_STORAGE_KEYS.STORE_CLIP_ID, storedId);
+	
+}
+
+//
+function handleRerenderStoreBtn(target) {
+	let $OnMadalClips = document.querySelectorAll('.youtube-search-modal-clip');
+	[ ...$OnMadalClips ].map(e => {
+		console.log(e.querySelector('.preview-container>iframe').src);
+	});
 }
 
 function handleEmojiEvent(target) {
