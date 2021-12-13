cfef04184fe28ae022e75d296b208c05baef4200
blacktost <dbstjd100@naver.com>
2021-12-13 08:18:41 +0000

    feat: snackBar 생성중

--- src/js/utils/setAtribute.js
+++ src/js/utils/setAtribute.js
@@ -1,13 +1,21 @@
-export const showElement = (tag) => {
-  tag.classList.remove("d-none");
+export const showElement = tag => {
+	tag.classList.remove('d-none');
 };
-export const hideElement = (tag) => {
-  tag.classList.add("d-none");
+export const hideElement = tag => {
+	tag.classList.add('d-none');
 };
 
-export const hideScroll = (tag) => {
-  tag.classList.add("scroll-none");
+export const hideScroll = tag => {
+	tag.classList.add('scroll-none');
 };
-export const showScroll = (tag) => {
-  tag.classList.remove("scroll-none");
+export const showScroll = tag => {
+	tag.classList.remove('scroll-none');
+};
+
+export const clickedBtnForBgColor = tag => {
+	tag.classList.add('bg-cyan-100');
+};
+
+export const unClickedBtn = tag => {
+	tag.classList.remove('bg-cyan-100');
 };
