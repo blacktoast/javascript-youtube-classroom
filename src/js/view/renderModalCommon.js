31a1512f3298ffa7e98ab04d80026696cbe6c880
blacktoast <dbstjd100@naver.com>
2021-12-14 13:50:59 +0000

    chore: html 구조변경

--- src/js/view/renderModalCommon.js
+++ src/js/view/renderModalCommon.js
@@ -1,7 +1,7 @@
 import { hideElement, showElement } from "../utils/setAtribute.js";
 import { $ } from "../utils/dom.js";
 
-const $skeleton = $("#skeleton-wrapper");
+const $skeleton = $(".youtube-search-modal__skeleton-wrapper");
 const $youtubeWrapper = $("#view-video-wrapper");
 
 export function renderLoading() {
